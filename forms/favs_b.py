76186841b7a9176b82849f371237d976b940315c
baoobab <75033176+baoobab@users.noreply.github.com>
2022-01-25 20:20:18 +0400

    some changes

--- forms/favs_b.py
+++ forms/favs_b.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, SubmitField, BooleanField
+from wtforms import PasswordField, StringField, SubmitField, BooleanField, IntegerField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired
 
 
 class FavsForm(FlaskForm):
-    ttle = StringField('', validators=[DataRequired()])
-    submit = SubmitField('\u2713 В избранном')
-    # submit2 = SubmitField('Добавить в избранное')
+    ttle = StringField('11', validators=[DataRequired()])
+    favs_id = IntegerField('23', validators=[DataRequired()])
+    submit2 = SubmitField('\u2713 В избранном')
+    submit = SubmitField('Добавить в избранное')
